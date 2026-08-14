0eaecd033bc7e44989905e36a735e87a324c32b5
j-haskins <jay.haskins@gmail.com>
2026-08-14 09:28:41 -0700

    fix(terraform): explicitly enable oidc_issuer on AKS cluster to prevent CreateOrUpdate error

--- terraform/modules/aks-gitops/main.tf
+++ terraform/modules/aks-gitops/main.tf
@@ -58,6 +58,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
+  oidc_issuer_enabled       = true
+
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
